d558a78d8e448994da6f235d345fe543108a4cb2
apogenes <apogenes@gmail.com>
2025-02-11 17:52:37 +0900

    Update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot, Root } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+// 전역 변수로 root를 선언
+let root: Root | null = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("root");
+
+  if (container && !root) {
+    root = createRoot(container);
+  }
+
+  if (root) {
+    root.render(
+      <StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </StrictMode>,
+    );
+  }
+});
