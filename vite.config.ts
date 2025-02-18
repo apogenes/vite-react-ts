9fafe53dfc3745e33a3858435403ead3596bd97c
apogenes <apogenes@gmail.com>
2025-02-18 18:23:34 +0900

    Add dotenv, port setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import withReactRouter from "vite-plugin-next-react-router";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
   plugins: [
@@ -15,4 +18,12 @@ export default defineConfig({
       layout: "_layout",
     }),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.VITE_PORT || '8090', 10),
+    hmr: {
+      overlay: false,
+    },
+    open: false,
+  }
 });
