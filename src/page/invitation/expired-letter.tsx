68d861aa4e0e49d2327894e0924cc87a07faf891
apogenes <apogenes@gmail.com>
2025-02-26 17:34:40 +0900

    react-hook-form, vite-svgr 추가

--- src/page/invitation/expired-letter.tsx
+++ src/page/invitation/expired-letter.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/src/shared/ui/button";
+import { Button } from "@/shared/ui/button";
 import { useNavigate } from "react-router-dom";
 
 export default function ExpiredInvitation() {
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import withReactRouter from "vite-plugin-next-react-router";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import svgr from "vite-plugin-svgr";
 
-dotenv.config()
+dotenv.config();
 
 export default defineConfig({
   plugins: [
@@ -17,13 +18,21 @@ export default defineConfig({
       extensions: ["ts", "tsx"],
       layout: "_layout",
     }),
+    svgr({
+      svgrOptions: {
+        svgo: true, // SVG 최적화 활성화
+        svgoConfig: {
+          plugins: [{ removeViewBox: false }], // viewBox 제거 방지
+        },
+      },
+    }),
   ],
   server: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.VITE_PORT || '8090', 10),
+    host: "0.0.0.0",
+    port: parseInt(process.env.VITE_PORT || "8090", 10),
     hmr: {
       overlay: false,
     },
     open: false,
-  }
+  },
 });
