4eaad077adf32ab630153eae017cc8d6b2ab2000
apogenes <apogenes@gmail.com>
2025-02-27 20:28:32 +0900

    dayjs 및 time 로직 추가

--- src/shared/util/time.ts
+++ src/shared/util/time.ts
@@ -1,3 +1,38 @@
+import dayjs from 'dayjs';
+
+type DateFormat = string | number | Date;
+
+export const getAge = (
+  birthDate: DateFormat | dayjs.Dayjs,
+  minAge?: number,
+) => {
+  const FULL_YEAR_FORMAT = 'YYYYMMDD';
+  const format = String(birthDate).length === 6 ? 'YYMMDD' : FULL_YEAR_FORMAT;
+  const today = dayjs();
+  const birthday = dayjs(birthDate, format);
+  const age = today.diff(birthday, 'years');
+  const diffDays = today.diff(birthday, 'days');
+
+  if (format === FULL_YEAR_FORMAT) {
+    if (diffDays < 0) {
+      // 나이계산 불가
+      return 0;
+    }
+
+    return age;
+  }
+
+  // 연도가 2자리일 경우에만 나이 보정함.
+  if (minAge !== 0 || !minAge) {
+    minAge = 15; // 나이 최소값 미설정일 때 기본 15세 (가맹점주 나이 기준)
+  }
+
+  if (diffDays >= 0 && age >= minAge) {
+    return age;
+  }
+
+  return today.diff(birthday.subtract(100, 'years'), 'years');
+};
 
 export const formatTime = (time: number) => {
   if (time < 0) {
